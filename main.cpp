da058a2074ba6b5b776a0d9318fb4462468d3867
nickol <cnickol@gmail.com>
2020-10-05 15:45:09 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@
 int main(int, char**)
 {
     std::cout << "build " << version() << std::endl;
-    std::cout << "Hello, World!" << version() << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
